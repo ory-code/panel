15ee5a289c75c5b5c913726293a9853097836114
sacha <orysacha@yahoo.be>
2022-03-18 18:09:00 +0100

    fetch fake product api

--- client/src/Pages/Products.jsx
+++ client/src/Pages/Products.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import SideBar from "../Components/SideBar"
+import NavBar from "../Components/NavBar"
+import "../Style/Pages/Products.scss"
 const Products = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        'https://fakestoreapi.com/products',
+      );
+      const json = await res.json();
+      setData(json);
+      console.log("data", json);
+    };
+    fetchData();
+  }, [setData]);
+
   return (
-    <div>Products</div>
+    <div className="products" >
+      <SideBar/>
+      <div className="products__container">
+        <NavBar />
+        <div className="top">
+        <h1>The Store </h1>
+        </div>
+        <div className="middle">
+          🏬 
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
   )
 }
 
